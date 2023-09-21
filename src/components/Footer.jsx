329b75776bdf5fddd431ffb85d5326af7b7931ef
R.Daniel Gimenez <dgimene@outlook.com>
2023-09-21 20:52:42 -0300

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Dirección: Avenida Providencia 1234, Providencia, Santiago</p>
+      <p>Teléfono: +56 22 123 456787</p>
+      <p>Email: contacto@laespañola.cl</p>
+      <small>2023. La Española. Todos los derechos reservados.</small>
+      <small>Esta es una aplicación ficticia</small>
+    </footer>
+  );
+}
+
+export default Footer;
