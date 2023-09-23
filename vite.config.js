de88646e6624db048e00ffca640e22d15c8856c9
R.Daniel Gimenez <dgimene@outlook.com>
2023-09-23 16:52:59 -0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/proy_4_UDD/"
+  base: "/proy_4_UDD/", //proyecto
+  build: {
+    assetsDir: 'assets' 
+  }
 })
